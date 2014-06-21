cb90c13ea3adc79ebb86b61069f7b5b94b568335
andrew-sv <andrew.sventytskyi@gmail.com>
2014-06-21 15:11:37 +0300

    Book Catalog sample

---
+++ public/js/data.js
@@ -0,0 +1,25 @@
+var data = [
+	{ name: 'jQuery 1.4 Reference Guide',
+	  icon: 'img/icon1.jpg',
+	  url:  'http://www.packtpub.com/jquery-1-4-reference-guide/book'},
+	{ name: 'Learning jQuery, Third Edition',
+	  icon: 'img/icon2.jpg',
+	  url:  'http://www.packtpub.com/learning-jquery-for-interaction-design-web-development-with-javascript/book'},
+	{ name: 'jQuery UI 1.8: The User Interface Library for jQuery',
+	  icon: 'img/icon3.jpg',
+	  url:  'http://www.packtpub.com/jquery-ui-1-8-user-interface-library/book'},
+	{ name: 'HTML5 Games Development by Example: Beginner’s Guide',
+	  icon: 'img/icon4.jpg',
+	  url:  'http://www.packtpub.com/html5-games-development-using-css-javascript-beginners-guide/book'},
+	{ name: 'Node Web Development',
+	  icon: 'img/icon5.jpg',
+	  url:  'http://www.packtpub.com/node-javascript-web-development/book'},
+	{ name: 'Object-Oriented JavaScript',
+	  icon: 'img/icon6.jpg',
+	  url:  'http://www.packtpub.com/object-oriented-javascript/book'}
+	
+];
+var template = '<li class="record">\
+					<img class="record-icon" src="{0}" />\
+					<a href="{1}" class="record-link">{2}</a>\
+				</li>';
---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var path = require('path');
+
+var server = express();
+
+server.use(express.static(path.join(__dirname, 'public'))); // static content
+
+server.listen(4000);
